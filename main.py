05636c3a790cd40e2fd863b87f495ab985b5ea42
DjordjeMarojevic <djordjemarojevic002@gmail.com>
2025-09-19 10:46:27 +0200

    zavrsen domaci

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -31,7 +32,8 @@ grad_map = {
     "Podgorica ": "Podgorica",
     "pg ": "Podgorica",
     "novi sad": "Novi Sad",
-    "NIŠ": "Niš"
+    "NIŠ": "Niš",
+    "Nis": "Niš"
  
 }
 
@@ -140,3 +142,117 @@ print("\nStudent sa najkraćim trajanjem studija:")
 print(student_min)
 
 
+df['Godine'] = pd.to_numeric(df['Godine'], errors='coerce')
+
+plt.hist(df['Godine'].dropna(), bins=20, edgecolor='black')
+
+plt.title("Raspodjela godina studenata")
+plt.xlabel("Godine")
+plt.ylabel("Broj studenata")
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+plt.savefig("histogram_godine.png")
+
+df['Prosek'] = pd.to_numeric(df['Prosek'], errors='coerce')
+
+plt.figure(figsize=(6, 8))
+plt.boxplot(df['Prosek'].dropna(), vert=True, patch_artist=True,
+            boxprops=dict(facecolor="lightblue", color="blue"),
+            medianprops=dict(color="red"),
+            whiskerprops=dict(color="black"),
+            capprops=dict(color="black"),
+            flierprops=dict(markerfacecolor="orange", marker="o", markersize=8))
+
+plt.title("Boxplot raspodjele prosjeka studenata")
+plt.ylabel("Prosjek")
+plt.grid(axis="y", linestyle="--", alpha=0.7)
+
+plt.savefig("boxplot_prosek.png")
+
+avg_by_city = df.groupby('Grad')['Prosek'].mean().sort_values(ascending=False)
+
+plt.figure(figsize=(10, 6))
+avg_by_city.plot(kind='bar', color='skyblue', edgecolor='black')
+
+plt.title("Prosjecan prosjek po gradu")
+plt.xlabel("Grad")
+plt.ylabel("Prosjecan prosjek")
+plt.xticks(rotation=45, ha='right')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+plt.tight_layout()
+plt.savefig("bar_chart_prosek_po_gradu.png")
+
+df['ESPB'] = pd.to_numeric(df['ESPB'], errors='coerce')
+
+plt.figure(figsize=(12, 6))
+df.boxplot(column="ESPB", by="Grad", grid=False,
+           boxprops=dict(color="blue"),
+           medianprops=dict(color="red"),
+           whiskerprops=dict(color="black"),
+           capprops=dict(color="black"),
+           flierprops=dict(markerfacecolor="orange", marker="o", markersize=6))
+
+plt.title("Raspodjela ESPB po gradovima")
+plt.suptitle("") 
+plt.xlabel("Grad")
+plt.ylabel("ESPB")
+plt.xticks(rotation=45, ha="right")
+
+plt.tight_layout()
+plt.savefig("boxplot_espb_po_gradovima.png")
+
+plt.figure(figsize=(8, 6))
+plt.scatter(df['Godine'], df['Prosek'], alpha=0.7, color="royalblue", edgecolors="k")
+
+plt.title("Godine vs. Prosjek")
+plt.xlabel("Godine")
+plt.ylabel("Prosjek")
+plt.grid(linestyle="--", alpha=0.6)
+
+plt.savefig("scatter_godine_vs_prosek.png")
+
+df['Datum_upisa'] = pd.to_datetime(df['Datum_upisa'], errors='coerce')
+
+df['Godina_upisa'] = df['Datum_upisa'].dt.year
+avg_by_year = df.groupby('Godina_upisa')['Prosek'].mean().dropna()
+plt.figure(figsize=(10, 6))
+plt.plot(avg_by_year.index, avg_by_year.values, marker='o', linestyle='-', color='blue')
+
+plt.title("Prosjecan prosjek po godini upisa")
+plt.xlabel("Godina upisa")
+plt.ylabel("Prosječan prosjek")
+plt.grid(linestyle="--", alpha=0.7)
+
+plt.savefig("linechart_prosek_po_godini_upisa.png")
+
+
+print("-----------------------------------------------------------------")
+
+df['Datum_upisa'] = pd.to_datetime(df['Datum_upisa'], errors='coerce')
+df['Godina_upisa'] = df['Datum_upisa'].dt.year
+
+pivot = pd.pivot_table(
+    df,
+    values='Prosek',
+    index='Grad',
+    columns='Godina_upisa',
+    aggfunc='mean'
+)
+
+print("Prosjecan prosjek po gradu i godini upisa")
+print(pivot)
+
+
+print()
+print("-------------------------------------")
+top5 = df.sort_values(by="Prosek", ascending=False).head(5)
+
+print("Top 5 studenata sa najvišim prosjekom:")
+print(top5[['ID', 'Ime', 'Prosek']])
+
+
+avg_espb_by_city = df.groupby('Grad')['ESPB'].mean().sort_values(ascending=False)
+
+print("Prosjecan broj ESPB bodova po gradu:")
+print(avg_espb_by_city)
